190d917cbf1fc1c179fcff0c49e62ecd12741a7b
Aravind <aravindpandiyarajan2004@gmail.com>
2024-03-13 14:00:09 +0530

    Aravind file added

---
+++ Aravind.java
@@ -0,0 +1,13 @@
+class Aravind{
+	static void display(){
+		System.out.println("Batch 5");
+		System.out.println("Working at relevantz");
+		System.out.println("intern-software engineer");
+	}
+	public static void main(String args[]){
+		System.out.println("Aravinda Rajan P");
+		display();
+
+	}
+}
+
